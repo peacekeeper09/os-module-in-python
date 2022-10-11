cdab2457b8e8ca23c4279c713784e41a03fa0376
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2022-10-11 15:40:02 +0530

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -10,14 +10,9 @@ if shutdown == 'yes':
 #if the input is yes, it will shutdown your system.
 
 #os.shutdown(s = 0, m = 0, h = 0) explained
-#s
-time to wait before shutting down (in seconds), added to m and h; passed to Sys.sleep
-
-m
-time to wait before shutting down (in minutes), added to s and h; passed to Sys.sleep
-
-h
-time to wait before shutting down (in hours), added to s and m; passed to Sys.sleep
+#s = time to wait before shutting down (in seconds), added to m and h
+#m = time to wait before shutting down (in minutes), added to s and h
+#h = time to wait before shutting down (in hours), added to s and m
 
 elif shutdown == 'no':
 #checks if the input given by the user is no.
